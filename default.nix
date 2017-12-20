ff137878bd2f58b57611a7aece328f9c854fdb06
David Asabina <vid@bina.me>
2017-12-20 22:10:47 +0100

    mimicking some example

--- default.nix
+++ default.nix
@@ -1,14 +1,20 @@
-{ stdenv, pkgs }:
+{ stdenv, fetchurl, dpkg }:
 
 stdenv.mkDerivation rec {
-  name = "bitscope";
-  src = ./.;
-  version = "0.1.0";
+  name = "bitscope-dso";
+  src = fetchurl {
+    url = "http://bitscope.com/download/files/bitscope-dso_2.8.FE22H_i386.deb";
+    sha256 = "f1058a015244dfcd04de3488873e130359150ff826c6b310d73018239e2a093f";
+  };
+  version = "2.8.0";
 
-  buildInputs = with pkgs; [
-  ];
+  buildInputs = [ dpkg ];
+  unpackPhase = ''
+    dpkg-deb -x ${src} ./
+  '';
 
-  #shellHook = ''
-  #''
-
+  shellHook = ''
+    echo "installed to ${src}"
+    echo "out is $out"
+  '';
 }
