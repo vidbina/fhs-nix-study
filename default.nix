76617b62906f7e69f6310f36afc5d6169428da96
David Asabina <vid@bina.me>
2017-12-21 02:43:42 +0100

    :cow::poop:-ing around

--- default.nix
+++ default.nix
@@ -1,20 +1,44 @@
 { stdenv, fetchurl, dpkg }:
 
 stdenv.mkDerivation rec {
-  name = "bitscope-dso";
-  src = fetchurl {
-    url = "http://bitscope.com/download/files/bitscope-dso_2.8.FE22H_i386.deb";
-    sha256 = "f1058a015244dfcd04de3488873e130359150ff826c6b310d73018239e2a093f";
-  };
-  version = "2.8.0";
+  name = "bitscope-dso-dev";
+  buildInputs = [
+    (stdenv.mkDerivation rec {
+      name = "bitscope-dso-${version}";
+      version = "2.8.0";
+    
+      src = fetchurl {
+        url = "http://bitscope.com/download/files/bitscope-dso_2.8.FE22H_i386.deb";
+        sha256 = "0d338g21rzknwgn5wannvkyy9aq37vlfqkppgjv3bkjqkxcyvv7a";
+      };
+    
+      buildInputs = [ dpkg ];
+    
+      #phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+    
+      unpackPhase = ''
+        dpkg-deb -x ${src} ./
+      '';
+    
+      dontBuild = true;
+    
+    #  buildPhase = ''
+    #    touch rm-build
+    #  '';
+    
+      installPhase = ''
+        ls -la .
+        mkdir -p "$out/bin"
+        cp -a usr/* "$out/"
+        echo "/////////////"
+        ls -la $out
+        #patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/bitscope-dso"
+        #patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$out/bin/start-bitscope-dso"
+      '';
+    })
+  ];
 
-  buildInputs = [ dpkg ];
-  unpackPhase = ''
-    dpkg-deb -x ${src} ./
-  '';
 
   shellHook = ''
-    echo "installed to ${src}"
-    echo "out is $out"
   '';
 }
